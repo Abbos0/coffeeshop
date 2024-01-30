f03c2b4944b1105858d361f9c0497ad4e558f958
Abbos <norqulovabbos1701@gmail.com>
2024-01-31 02:28:10 +0500

    sdsd
    sdsdsd

--- script.js
+++ script.js
@@ -3,37 +3,26 @@ function Toggle(){
     Navbar.classList.toggle("toggle")
   }
 
+// ===================================Contact===============================
+const Username = document.querySelector("#username")
+const Email = document.querySelector("#email")
+const Call = document.querySelector("#call")
+const Form = document.querySelector("#form")
 
-
-
-  const Form = document.querySelector(".form")
-  const Name = document.querySelector(".name")
-  const Email = document.querySelector(".email")
-  const Call = document.querySelector(".call")
-  const Input = document.querySelector("#input")
-  const Submit = document.querySelector(".submit")
-
-
-
-  const changeHandler = ()=>{
-    const SecretKey = '8933fcfd1e22f162fc583a46dfa2d836'
-    fetch(`https://api.openweathermap.org/data/2.5/form?q=${Input}&appid=${SecretKey}`)
-        .then(res=>res.json())
-        .then(data=>{
-            console.log(data);
-            Name.innerHTML = data.name;
-            Email.innerHTML = data.email;
-            Call.innerHTML = data.call;
-            SendMessage(data)
-        })
+const GetValue = (event) =>{
+    event.preventDefault()
+    console.log(Username.value, Email.value, Call.value);
 }
-  const SendMessage = (fore) =>{
-    search = `%0A 👦Name: ${fore.name}%0A 📧 Email: ${fore.email} %0A 📞 Call: ${fore.call}   `
-    chat_id = -1002128588085
-    token = `6834109969:AAEhUkHL4MsMs8Be2CWGY9oC7KXSbW8JHAM`
-    url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${search}&parse_mode=html`
-
+const SendMessage = (e) =>{
+    e.preventDefault()
+    text = `%0A 👦 Username: ${Username.value}  %0A 📩Email:  ${Email.value} %0A 📞 Phone: ${Call.value}  `
+    chat_id = -1002128588085 ,token = `6834109969:AAEhUkHL4MsMs8Be2CWGY9oC7KXSbW8JHAM`,url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${text}&parse_mode=html`
     let api = new   XMLHttpRequest();
     api.open("GET", url, true);
-    api.send();
-}   
+    api.send();Username.value = "" ,Email.value = "",Call.value = "";
+}  
+Form.addEventListener("submit" , SendMessage )
+
+// ===================================Contact===============================
+
+
