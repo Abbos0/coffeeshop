f6c3a8c04babcdf49a06084518562d59151a5862
Abbos <norqulovabbos1701@gmail.com>
2024-06-18 13:45:29 +0500

    sds
    sds

--- script.js
+++ script.js
@@ -5,6 +5,7 @@ function Toggle(){
 
 // ===================================Contact===============================
 const Username = document.querySelector("#username")
+const Sitename = document.querySelector("#sitename")
 const Email = document.querySelector("#email")
 const Call = document.querySelector("#call")
 const Form = document.querySelector("#form")
@@ -15,11 +16,11 @@ const GetValue = (event) =>{
 }
 const SendMessage = (e) =>{
     e.preventDefault()
-    text = `%0A 👦 Username: ${Username.value}  %0A 📩Email:  ${Email.value} %0A 📞 Phone: ${Call.value}  `
+    text = ` %0A 👦 Username: ${Sitename.value}  %0A 👦 Username: ${Username.value}  %0A 📩Email:  ${Email.value} %0A 📞 Phone: ${Call.value}  `
     chat_id = -1002128588085 ,token = `6834109969:AAEhUkHL4MsMs8Be2CWGY9oC7KXSbW8JHAM`,url = `https://api.telegram.org/bot${token}/sendMessage?chat_id=${chat_id}&text=${text}&parse_mode=html`
     let api = new   XMLHttpRequest();
     api.open("GET", url, true);
-    api.send();Username.value = "" ,Email.value = "",Call.value = "";
+    api.send();Sitename.value = "" ,Username.value = "" ,Email.value = "",Call.value = "";
 }  
 Form.addEventListener("submit" , SendMessage )
 
